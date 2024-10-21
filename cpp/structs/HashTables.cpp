9919416ae6676d0e6e5138943fcf92ed5157116a
pedrotokar <pedrotokar2004@gmail.com>
2024-10-21 10:20:39 -0300

    Add header file for hashtables and transform them into templates

--- cpp/structs/HashTables.cpp
+++ cpp/structs/HashTables.cpp
@@ -1,99 +1,93 @@
 #include <iostream>
+#include "HashTables.h"
 #define displayKey(table, k) {if (table.get(k) == nullptr) {cout << "Não há valor associado à chave " << k << endl;} else {cout << "[" << k << "]: " <<  table.get(k)->value << endl;};}
 
 using namespace std;
 
-typedef struct HashTableNode HTNode;
-struct HashTableNode {
-    unsigned key;
-    int value;
-    HTNode* next;
-    HTNode* previous;
-};
-
-class HashTable {
-    private:
-        HTNode** m_table; //An array with the respective lists for each hash
-        int m_size; //The size of the hash function image set;
-
-        unsigned hash(unsigned key) {
+template <typename T>
+unsigned HashTable<T>::hash(unsigned key) {
             return key % m_size;
         }
 
-    public:
-        HashTable(int iM): m_table(nullptr), m_size(iM) { //iM is hash image size
-                m_table = new HTNode*[iM]; //initializes as an array (before initialized as nullptr)
-                for (int i = 0; i < m_size; i++) { //will set all the values to nullptr since no insertion has been made
-                    m_table[i] = nullptr;
-                }
-            }
+template <typename T>
+HashTable<T>::HashTable(int iM): m_table(nullptr), m_size(iM) { //iM is hash image size
+    m_table = new HTNode<T>*[iM]; //initializes as an array (before initialized as nullptr)
+    for (int i = 0; i < m_size; i++) { //will set all the values to nullptr since no insertion has been made
+        m_table[i] = nullptr;
+    }
+}
 
-        ~HashTable() {
-            for (int i = 0; i < m_size; i++) { //Will delete every hash stored values
-                HTNode * node = m_table[i];
-                while (node != nullptr) { //Delete the entire linked list of that hash value
-                    HTNode* nextNode = node->next;
-                    delete node;
-                    node = nextNode;
-                }
-            }
-            delete[] m_table; //Delete hashes array
+template <typename T>
+HashTable<T>::~HashTable() {
+    for (int i = 0; i < m_size; i++) { //Will delete every hash stored values
+        HTNode<T>* node = m_table[i];
+        while (node != nullptr) { //Delete the entire linked list of that hash value
+            HTNode<T>* nextNode = node->next;
+            delete node;
+            node = nextNode;
         }
+    }
+    delete[] m_table; //Delete hashes array
+}
 
-        void set(unsigned key, int value){ //For seting a pair of key, value
-            unsigned hashed = hash(key); //Gets the hash for the key
-            HTNode* ptrNode = m_table[hashed]; //Gets the linked list of that hash
-            while (ptrNode != nullptr and ptrNode->key != key){ //And runs till find the node repecting to the key
-                ptrNode = ptrNode->next;
-            }
-            if (ptrNode == nullptr){ //The key has never been used, so creates the node
-                ptrNode = new HTNode;
-                ptrNode->key = key;
-                ptrNode->previous = nullptr;
-                ptrNode->next = m_table[hashed];
-                if (m_table[hashed] != nullptr){
-                    m_table[hashed]->previous = ptrNode;
-                } //Inserts before the list (doesn't need to add in the end)
-                m_table[hashed] = ptrNode;
-            }
-            ptrNode->value = value; //in either cases, update the value
-        }
+template <typename T>
+void HashTable<T>::set(unsigned key, T value){ //For seting a pair of key, value
+    unsigned hashed = hash(key); //Gets the hash for the key
+    HTNode<T>* ptrNode = m_table[hashed]; //Gets the linked list of that hash
+    while (ptrNode != nullptr and ptrNode->key != key){ //And runs till find the node repecting to the key
+        ptrNode = ptrNode->next;
+    }
+    if (ptrNode == nullptr){ //The key has never been used, so creates the node
+        ptrNode = new HTNode<T>;
+        ptrNode->key = key;
+        ptrNode->previous = nullptr;
+        ptrNode->next = m_table[hashed];
+        if (m_table[hashed] != nullptr){
+            m_table[hashed]->previous = ptrNode;
+        } //Inserts before the list (doesn't need to add in the end)
+        m_table[hashed] = ptrNode;
+    }
+    ptrNode->value = value; //in either cases, update the value
+}
 
-        HTNode* get(unsigned key){
-            unsigned hashed = hash(key);
-            HTNode* ptrNode = m_table[hashed];
-            while (ptrNode != nullptr and ptrNode->key != key){ //And runs till find the node repecting to the key
-                ptrNode = ptrNode->next;
-            }
-            return ptrNode;
-        }
+template <typename T>
+HTNode<T>* HashTable<T>::get(unsigned key){
+    unsigned hashed = hash(key);
+    HTNode<T>* ptrNode = m_table[hashed];
+    while (ptrNode != nullptr and ptrNode->key != key){ //And runs till find the node repecting to the key
+        ptrNode = ptrNode->next;
+    }
+    return ptrNode;
+}
 
-        void remove(unsigned key){
-            unsigned hashed = hash(key);
-            HTNode* ptrNode = m_table[hashed];
-            while (ptrNode != nullptr and ptrNode->key != key){ //And runs till find the node repecting to the key
-                ptrNode = ptrNode->next;
-            }
-            if (ptrNode != nullptr){
-                HTNode* nextNode = ptrNode->next;
-                if (nextNode != nullptr) {
-                    nextNode->previous = ptrNode->previous;
-                }
-                HTNode* previousNode = ptrNode->previous;
-                if (previousNode != nullptr) {
-                    ptrNode->previous->next = ptrNode->next;
-                } else {
-                    m_table[hashed] = ptrNode->next;
-                }
-                delete ptrNode;
-            } //if reaches here, then the key didn't exist anyway
+template <typename T>
+void HashTable<T>::remove(unsigned key){
+    unsigned hashed = hash(key);
+    HTNode<T>* ptrNode = m_table[hashed];
+    while (ptrNode != nullptr and ptrNode->key != key){ //And runs till find the node repecting to the key
+        ptrNode = ptrNode->next;
+    }
+    if (ptrNode != nullptr){
+        HTNode<T>* nextNode = ptrNode->next;
+        if (nextNode != nullptr) {
+            nextNode->previous = ptrNode->previous;
         }
-};
+        HTNode<T>* previousNode = ptrNode->previous;
+        if (previousNode != nullptr) {
+            ptrNode->previous->next = ptrNode->next;
+        } else {
+            m_table[hashed] = ptrNode->next;
+        }
+        delete ptrNode;
+    } //if reaches here, then the key didn't exist anyway
+}
+
+template class HashTable<int>;
 
 //TODO: adicionar algortimos de resize da hashtable
 
 /*int main(){
-    HashTable hashnew = HashTable(20);
+    HashTable<int> hashnew = HashTable<int>(20);
 
     cout << "Acessando chave inválida" << endl;
     displayKey(hashnew, 20);
